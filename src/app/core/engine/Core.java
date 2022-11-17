cd7f1d5328580662083fb00f9253c289981ab469
dpv927 <dpv927@inlumine.ual.es>
2022-11-17 21:43:52 +0100

    App Core

---
+++ src/app/core/engine/Core.java
@@ -0,0 +1,10 @@
+package app.core.engine;
+
+public abstract class Core {
+	
+	/* Funciones de un core (MakeMove)	
+	 	--> Calcular proximo movimiento
+		--> Modificar el tablero tras realizar calculos
+		--> Controlar que el usuario no pueda tocar el tablero mientras se realizan calculos
+	*/
+}
