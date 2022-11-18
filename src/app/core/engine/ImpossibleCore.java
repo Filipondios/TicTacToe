04135580b945289354065358936df7d5cd43eef4
dpv927 <dpv927@inlumine.ual.es>
2022-11-19 00:36:28 +0100

    Impliement Core methods

--- src/app/core/engine/ImpossibleCore.java
+++ src/app/core/engine/ImpossibleCore.java
@@ -1,5 +1,17 @@
 package app.core.engine;
 
+import app.core.board.Board;
+
 public class ImpossibleCore extends Core {
+
+	/** Crea un ImpossibleCore (core de dificultad imposible) y asicia a este un tablero.
+	 * @param board  Tablero donde se van a realizar todos los movimientos durante la partida. */
+	public ImpossibleCore(Board board) {
+		super(board);
+	}
 	
+	@Override
+	public void makeMove() {
+		
+	}	
 }
--- src/app/core/engine/NormalCore.java
+++ src/app/core/engine/NormalCore.java
@@ -1,5 +1,17 @@
 package app.core.engine;
 
+import app.core.board.Board;
+
 public class NormalCore extends Core{
+
+	/** Crea un NormalCore (core de dificultad normal) y asicia a este un tablero.
+	 * @param board  Tablero donde se van a realizar todos los movimientos durante la partida. */
+	public NormalCore(Board board) {
+		super(board);
+	}
 	
+	@Override
+	public void makeMove() {
+		
+	}	
 }
