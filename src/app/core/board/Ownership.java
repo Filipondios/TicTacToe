0c9a5f5048035ecca88232360f7c7832ae551dfd
Filipondios <danialhamilla262@gmail.com>
2022-11-27 23:41:08 +0100

    Ownership Documentation

--- src/app/core/board/Ownership.java
+++ src/app/core/board/Ownership.java
@@ -1,16 +1,13 @@
 package app.core.board;
 
 /**
- * <p>
- * Enum que tiene 3 posibles constantes. Representa cada uno de los estados o
- * valores de
- * una celda del tablero. Estos pueden ser <i>NONE</i> si la celda esta vacia,
- * <i>AI</i> si el motor de la aplicacion marco la celda y <i>PLAYER</i> si
- * fue el jugador.
- * <p>
- * 
- * @author Filipondios
- * @version 17.11.2022
+ * <p> Enum that has 3 possible constants. Represents each of the states or
+ * values ​​of a cell in a board. These can be <i>NONE</i> if the cell is empty,
+ * <i>AI</i> if the application engine marked the cell and <i>PLAYER</i> if
+ * it was the player. </p>
+ *
+ * @author Filippondios
+ * @version 27.11.2022
  */
 public enum Ownership {
 	AI,
