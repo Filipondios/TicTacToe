fa6ea135e6a72415819dc808d530f4e35b78ba34
dpv927 <dpv927@inlumine.ual.es>
2022-11-27 16:34:50 +0100

    Game core

--- src/app/core/engine/Core.java
+++ src/app/core/engine/Core.java
@@ -1,21 +1,6 @@
 package app.core.engine;
 
-import app.core.board.Board;
-
 public abstract class Core {
-
-	/** Tablero donde se van a realizar todos los movimientos durante la partida. */
-	private Board board;
-
-	/**
-	 * Crea un Core y asicia a este un tablero.
-	 * 
-	 * @param board Tablero donde se van a realizar todos los movimientos 
-	 * 				durante la partida.
-	 */
-	public Core(Board board) {
-		this.board = board;
-	}
 
 	/**
 	 * Funcion principal de un <i>Core</i>. Debe seguir los siguientes
@@ -29,8 +14,4 @@ public abstract class Core {
 	 * @param board Tablero en el que se va a hacer el movimiento.
 	 */
 	public abstract void makeMove();
-
-	public Board getBoard() {
-		return this.board;
-	}
 }
