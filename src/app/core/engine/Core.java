3f21fc6ff7ee4017214fb42845407678607a1223
Filipondios <danialhamilla262@gmail.com>
2022-11-28 16:01:45 +0100

    Core Documentation

--- src/app/core/engine/Core.java
+++ src/app/core/engine/Core.java
@@ -3,10 +3,15 @@ package app.core.engine;
 /**
  * <p>Interface that indicates how a Core must handle the calculations and requests
  * of a game board. It has a main function that calculates and performs the
- * moves that a core sees as the "best".</p>
+ * moves that a core sees as the <i>"best"</i>.</p>
+ * 
+ * <p>Depending on the Core, the <i>"best"</i> move depends on the hardness that a Core 
+ * wants to represent. For example, if a Core generates a move that its lightly based on
+ * a random decision, the most possible thing is that it won't be the best move for a 
+ * Core that searches the real best move in the board.</p>
  * 
  * @author Filipondios
- * @version 27.11.2022
+ * @version 28.11.2022
  * @see #makeMove()
  */
 public interface Core {
@@ -15,8 +20,8 @@ public interface Core {
 	 * <p>Principal function of a Core. A Core must follow the next requirements:</p>
 	 * <ul>
   	 * 	<li>Calculate movement on the board</li>
-  	 *	<li>Modify the dashboard after performing calculations</li>
+  	 *	<li>Modify the game board after performing calculations</li>
 	 * </ul> 
 	 */
-	public void makeMove();
+	void makeMove();
 }
