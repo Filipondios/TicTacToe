983ee6989aa47464339813d6b4c51ba4c2cd7aed
Filipondios <danialhamilla262@gmail.com>
2022-11-28 16:02:00 +0100

    Easy Core Documentation

--- src/app/core/engine/EasyCore.java
+++ src/app/core/engine/EasyCore.java
@@ -4,23 +4,29 @@ import app.core.board.Ownership;
 import app.frames.MainFrame;
 
 /**
- * Core que se encarga del modo de juego facil. Calcula una posicion del tablero
- * aleatoria que no este ocupada y la marca.
- * 
+ * Easy level Core. Generates a random number that represents the index of a cell in
+ * a board and if it's free, marks it as AI's property. For more info about this 
+ * implementation, see the makeMove method at this class.
+ *  
  * @author Filipondios
- * @version 18.11.2022
+ * @version 28.11.2022
+ * @see #makeMove()
  */
 public final class EasyCore implements Core {
 
+	/**
+	 * Starts a loop where generates a random number that represents the index of a cell
+	 * in a board. If calculated cell is occupied, the loop continues, otherwise, the cell
+	 * is marked as AI's property and exists the loop.
+	 */
 	@Override
 	public void makeMove() {
 		boolean let = true;
-		int chip = 0;
+		int chip;
 
-		while (let) { // Mientas la casilla calculada este ocupada, seguir el loop
-			chip = (int) (Math.random() * 8); // Generar casilla del tablero aleatoria
-			let = (MainFrame.game_board.markTile(chip, Ownership.AI) == -1) ? true : false; // Comprobar si esta ocupada
+		while (let) { // While the calculated cell is occupied, stay in the loop
+			chip = (int) (Math.random()*8); // Generate a random index of a board cell (0-8)
+			let = (MainFrame.game_board.markTile(chip, Ownership.AI) == -1); //Check if its occupied
 		}
-		MainFrame.game_board.markTile(chip, Ownership.AI); // Marcar casilla como IA (machine)
 	}
 }
