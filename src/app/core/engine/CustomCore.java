e8ad46d6cf9183e451ba12300f719973e8ce9a35
dpv927 <91225771+Filipondios@users.noreply.github.com>
2022-11-30 00:08:54 +0100

    Start Custom Core implementation

---
+++ src/app/core/engine/CustomCore.java
@@ -0,0 +1,19 @@
+package app.core.engine;
+
+/**
+ * @author Filipondios
+ * @version 30.11.2022
+ */
+public final class CustomCore implements Core {
+
+	private final int MAX_DEPTH;
+	
+	public CustomCore(int depth) {
+		this.MAX_DEPTH = depth;
+	}
+	
+	@Override
+	public void makeMove() {
+		
+	}	
+}
